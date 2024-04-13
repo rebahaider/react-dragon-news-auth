4962dc9392e53e055ff7bbcc0b7307b4f6f23614
rebahaider <rebahaiderema@gmail.com>
2024-04-13 12:54:25 +0600

    added a login route

--- src/Pages/LeftSideNavbar/LeftSideNavbar.jsx
+++ src/Pages/LeftSideNavbar/LeftSideNavbar.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 
 const LeftSideNavbar = () => {
+    const [categories, setCategory] = useState([]);
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategory(data))
+    }, [])
+
     return (
-        <div className='border'>
-            <h2>This is left side</h2>
+        <div className='border space-y-4'>
+            <h2 className="font-bold">All Category</h2>
+            {
+                categories.map(category =>
+                    <NavLink to={`/category/${category.id}`} className="block" key={category.id}>{category.name}</NavLink>
+                )
+            }
         </div>
     );
 };
